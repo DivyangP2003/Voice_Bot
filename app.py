03cb2af824520dc463d88e85d8e6058e368060c7
DivyangP2003 <divyangdpalshetkar@gmail.com>
2025-06-18 13:15:33 +0530

    app.py

--- app.py
+++ app.py
@@ -73,11 +73,11 @@ def load_groq_client():
 groq_client = load_groq_client()
 
 # ---- TITLE ----
-st.title("## 🤖 EchoMind: Your Personal AI Voice Assistant")
-# st.markdown("""
-# Speak your question — whether it’s about your resume, achievements, or general knowledge —  
-# **EchoMind** will listen, understand, and respond in your voice and tone.
-# """)
+st.title("🤖 EchoMind: Your Personal AI Voice Assistant")
+st.markdown("""
+Speak your question — whether it’s about your resume, achievements, or general knowledge —  
+**EchoMind** will listen, understand, and respond in your voice and tone.
+""")
 
 
 # ---- FUNCTIONS ----
